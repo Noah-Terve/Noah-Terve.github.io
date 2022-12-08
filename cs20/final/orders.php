53d895c1815b15b6b57d0c6e74ca03a5dcc2b987
hbenatar <helena.benatar@tufts.edu>
2022-12-08 11:10:08 -0500

    added cart page

--- cs20/final/orders.php
+++ cs20/final/orders.php
@@ -35,7 +35,7 @@ session_start();
                 <li><a href="./recipes.php">Recipes</a></li>
                 <?php
                     if (empty($_SESSION['username'])) echo ("<li><a href=\"./login.php\">Log In</a></li>");
-                    else echo("<li><a href=\"./orders.php\">My Orders</a></li><li><a href=\"./logout.php\">Log Out</a></li>");
+                    else echo("<li><a href=\"./orders.php\">My Cart</a></li><li><a href=\"./orders.php\">My Orders</a></li><li><a href=\"./logout.php\">Log Out</a></li>");
                 ?>
             </ul>
         </div>
